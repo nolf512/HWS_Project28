dc6e88ee7038eb224189897fa435f78c95c71f34
nofprog <k.jin3217@gmail.com>
2021-05-17 17:30:39 +0900

    メッセージの表示・非表示

--- HWS_Project28/ViewController.swift
+++ HWS_Project28/ViewController.swift
@@ -13,14 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Nothing to see here"
+        
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil)
 }
 
     @IBAction func authenticationTapped(_ sender: Any) {
-        
-        
+        unlockSecretMessage()
     }
     
     @objc func adjustForKeyboard(notification: Notification){
@@ -40,6 +42,27 @@ class ViewController: UIViewController {
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
         
+    }
+    
+    
+    func unlockSecretMessage(){
+        secret.isHidden = false
+        title = "Secret stuff"
+        
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage"){
+        secret.text = text
+        }
+        //secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+
+    }
+    
+    @objc func saveSecretMessage(){
+        guard secret.isHidden == false else { return }
+        
+        KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+        secret.resignFirstResponder()
+        secret.isHidden = true
+        title = "Nothing to see here"
     }
     
 }
